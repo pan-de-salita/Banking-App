27a3e0d2b46cc446c7f24a773983b9a5ec9eaa15
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-03-14 17:30:01 +0800

    style: add responsiveness to Root

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -4,7 +4,7 @@ export default function Root() {
   return (
     <>
       <div className='w-screen h-screen flex flex-col-reverse md:flex-row lg:flex-row'>
-        <nav className='p-8 flex flex-row md:flex-col lg:flex-col justify-start gap-4 outline-2 outline-dashed outline-blue-500'>
+        <nav className='p-8 flex md:flex-col lg:flex-col justify-start gap-4 outline-2 outline-dashed outline-blue-500'>
           <h2 className='invisible lg:visible'>R O O T</h2>
           <ul className='flex flex-row md:flex-col lg:flex-col gap-4'>
             <li>
