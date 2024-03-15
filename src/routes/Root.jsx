50d0bd4728aa53236f06de6374ac6cea6fed71bf
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-03-15 13:40:15 +0800

    chore: refactor Link classNames

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -14,42 +14,42 @@ export default function Root() {
     return location.pathname === path;
   }
 
+  function getLinkClass(path, isActivePath) {
+    const baseClasses = 'flex items-center gap-2 text-sm md:text-xl lg:text-xl hover:text-white transition-transform duration-200 ease-in-out';
+    const textColor = isActivePath(path) ? 'text-white' : 'text-[#A9A9AB]';
+    const translateY = isActivePath(path) ? '' : 'hover:translate-y-[-4px]';
+    return `${baseClasses} ${textColor} ${translateY}`;
+  }
+
   return (
     <>
       <div className='w-screen h-screen flex flex-col-reverse hd:flex-row lg:flex-row'>
-        <nav className='w-screen md:w-1/3 lg:w-1/5 p-5 md:p-10 lg:p-10 flex md:flex-col lg:flex-col justify-start gap-4 bg-[#17171B]'>
+        <nav className='w-screen md:w-1/3 lg:w-1/5 p-5 md:p-10 lg:p-11 flex md:flex-col lg:flex-col justify-start gap-4 bg-[#17171B]'>
           <SiGnubash className='hidden md:block lg:block' size={85} />
+          {/*  TODO: refactor using .map */}
           <ul className='md:h-screen lg:h-screen flex flex-row md:flex-col lg:flex-col gap-8 md:pt-[3.8rem] lg:pt-[3.8rem]'>
             <li>
-              <Link
-                className={`flex items-center gap-2 text-sm md:text-xl lg:text-xl hover:text-white ${isActive('/root/create-user') ? 'text-white' : 'text-[#A9A9AB]'}`}
-                to='/root/create-user'>
+              <Link className={getLinkClass('/root/create-user', isActive)} to='/root/create-user'>
                 <CgProfile className='hidden md:block lg:block' size={25} />
-                <span className='transition-transform duration-200 ease-in-out hover:translate-x-1'>Create User</span>
+                Users
               </Link>
             </li>
             <li>
-              <Link
-                className={`flex items-center gap-2 text-sm md:text-xl lg:text-xl hover:text-white ${isActive('/root/deposit') ? 'text-white' : 'text-[#A9A9AB]'}`}
-                to='/root/deposit'>
+              <Link className={getLinkClass('/root/deposit', isActive)} to='/root/deposit'>
                 <TbStackPush className='hidden md:block lg:block' size={25} />
-                <span className='transition-transform duration-200 ease-in-out hover:translate-x-1'>Deposit</span>
+                Deposit
               </Link>
             </li>
             <li>
-              <Link
-                className={`flex items-center gap-2 text-sm md:text-xl lg:text-xl hover:text-white ${isActive('/root/create-user') ? 'text-white' : 'text-[#A9A9AB]'}`}
-                to='/root/create-user'>
+              <Link className={getLinkClass('/root/create-user', isActive)} to='/root/create-user'>
                 <LuGitPullRequestDraft className='hidden md:block lg:block' size={25} />
-                <span className='transition-transform duration-200 ease-in-out hover:translate-x-1'>Withdraw</span>
+                Withdraw
               </Link>
             </li>
             <li>
-              <Link
-                className={`flex items-center gap-2 text-sm md:text-xl lg:text-xl hover:text-white ${isActive('/root/create-user') ? 'text-white' : 'text-[#A9A9AB]'}`}
-                to='/root/create-user'>
+              <Link className={getLinkClass('/root/create-user', isActive)} to='/root/create-user'>
                 <FiSend className='hidden md:block lg:block' size={25} />
-                <span className='transition-transform duration-200 ease-in-out hover:translate-x-1'>Send</span>
+                Send
               </Link>
             </li>
             <li className='hidden md:block md:h-[20rem] lg:block lg:h-[34rem]'>
@@ -57,11 +57,9 @@ export default function Root() {
             </li>
             <li>
               <div className='flex justify-between'>
-                <Link
-                  className={`flex items-center gap-2 text-sm md:text-xl lg:text-xl hover:text-white ${isActive('/root/create-user') ? 'text-white' : 'text-[#A9A9AB]'}`}
-                  to='/root/deposit'>
+                <Link className={getLinkClass('/root/create-user', isActive)} to='/root/create-user'>
                   <RiLogoutCircleRLine className='hidden md:block lg:block' size={25} />
-                  <span className='transition-transform duration-200 ease-in-out hover:translate-x-1'>Logout</span>
+                  Logout
                 </Link>
                 <Link
                   className={`flex items-center gap-2 text-sm md:text-xl lg:text-xl hover:text-white ${isActive('/root/create-user') ? 'text-white' : 'text-[#A9A9AB]'}`}
@@ -75,7 +73,7 @@ export default function Root() {
         <div className='p-8 w-full h-screen flex justify-center items-center'>
           <Outlet />
         </div>
-      </div >
+      </div>
     </>
   );
 }
